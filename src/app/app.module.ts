937257cb52bcfde6d5c6d8c81dbaf6cdff33295c
nuwana24 <nuwana24@gmail.com>
2023-03-26 21:53:33 +0530

    folder-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { RouterModule, Routes } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import {HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    SideNavComponent,
+    SignUpComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
